101da34b70dde684fc8b72aff47ae1e7d08e72c1
nimrosh <nimrodka@mta.ac.il>
2024-04-21 17:26:48 +0300

    first commit

--- q5.py
+++ q5.py
@@ -6,7 +6,18 @@ data = pd.read_csv('customerData.csv')
 #create a new dataframe with only people who are older than 38 and younger then 50
 data2 = data[(data['age'] > 38) & (data['age'] < 50)]
 #q6 
+# a
 # data3 should have all the people who are older than 50 and only numeric columns
 data3 = data[(data['age'] > 50)]
 data3 = data3.select_dtypes(include='number')
 print(data3)
+# b
+data4 = data[(data['age'] > 50)]
+data4 = data4.select_dtypes(include='number')
+data4.columns = range(data4.shape[1])
+print(data4)
+#q7
+data5 = pd.DataFrame(data4[3]).head(100)
+print(data5)
+
+
