0e115e89e492f2230798b4dc3876b2301f1fb038
nimrosh <nimrodka@mta.ac.il>
2024-05-05 11:41:51 +0300

    first commit

--- q10.py
+++ q10.py
@@ -1,10 +1,15 @@
 import pandas as pd
 #load the data
-data = pd.read_csv('customerData.csv')
-df = data[['housing_type', 'gender']]
-df_females = df[df['gender'] == 'F']
-df_females = df['housing_type'].value_counts()
-print('most popular female housing type',df_females.max())
-df_male = df[df['gender'] == 'M']
-df_male = df['housing_type'].value_counts()
-print('most popular male housing type',df_male.max())   
+df = pd.read_csv('customerData.csv')
+
+group10 = df[['gender','housing_type']]
+#Q10a
+group10_female = group10[group10['gender']=='F']
+group10_female = group10_female.groupby('housing_type').size()
+most_popular_housing_type = group10_female.idxmax()
+print(most_popular_housing_type)
+#Q10b
+man_group = df[df["gender"] == "M"]
+group = man_group.groupby("housing_type").size()
+most_popular_housing_type_male = group.idxmax()
+print(most_popular_housing_type_male)
